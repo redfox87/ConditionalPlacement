dc2489f6df7a9c64deb34197048ef8a592eb7114
Thomas Bloss <runredfox@gmail.com>
2015-04-24 00:01:08 -0400

    swift conditional placement code...(Error)

--- ConditionalPlacement-Objc/ConditionalPlacement-Objc/AppDelegate.m
+++ ConditionalPlacement-Objc/ConditionalPlacement-Objc/AppDelegate.m
@@ -18,7 +18,26 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
     // Insert code here...
+    int karl = 10;
+    int leonard = 7;
+    int karlPlace, leonardPlace;
     
+    karlPlace = karl > 7 ? 1 : 2;
+    leonardPlace = leonard > 7 ? 1 : 2;
+    
+    if (leonardPlace == 1 && karlPlace == 1 ) {
+        NSLog(@"Both teachers are placed 1");
+    }
+    else if(karlPlace == 1 && leonardPlace > 1){
+        NSLog(@"Karl is placed 1 and Leonard is placed 2");
+    }
+    else if (leonardPlace == 1 && karlPlace >1){
+        NSLog(@"Leonard is placed 1 and Karl is placed 2");
+    }
+    else {
+        NSLog(@"Karl and Leonard are placed 2");
+    }
+
     
     return YES;
 }
--- ConditionalPlacement-Swift/ConditionalPlacement-Swift/AppDelegate.swift
+++ ConditionalPlacement-Swift/ConditionalPlacement-Swift/AppDelegate.swift
@@ -6,22 +6,37 @@
 //  Copyright (c) 2014 DevMountain. All rights reserved.
 //
 
+
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-                            
+    
     var window: UIWindow?
-
-
+    
+    
     func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
-
         // Insert code here...
+        var karl = 10
+        var leonard = 7
         
+        var karlPlace: Int
+        var leonardPlace: Int
         
+        karlPlace = karl > 7 ? 1 : 2
+        leonardPlace = leonard > 7 ? 1 : 2
+        
+        if (karlPlace == leonardPlace) && (karlPlace == 1 ) {
+            println("Both teachers are placed 1")
+        } else if (karlPlace == 1) {
+            println("Karl is placed 1 and Leonard is placed 2")
+        } else if (leonardPlace == 1) {
+            println("Leonard is placed 1 and Karl is placed 2")
+        } else {
+            println("Karl and Leonard are placed 2")
+        }
         
         return true
     }
-
+    
 }
-
